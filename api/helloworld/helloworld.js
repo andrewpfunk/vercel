2b6751fbf7bd86b3f60fece0c469ace26e493e4e
andrewpfunk <andrewpfunk@gmail.com>
2025-07-13 17:57:00 -0400

    Delete api/helloworld/helloworld.js

--- api/helloworld/helloworld.js
+++
@@ -1,6 +0,0 @@
-export async function GET(request) {
-  // const response = await fetch('https://api.vercel.app/products');
-  // const products = await response.json();
-  const products = {"message":"Hello world!"};
-  return Response.json(products);
-}
