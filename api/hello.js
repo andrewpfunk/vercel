988847997355666932c5692a15ec6ce52f751466
andrewpfunk <andrewpfunk@gmail.com>
2025-07-13 18:08:00 -0400

    Update hello.js

--- api/hello.js
+++ api/hello.js
@@ -1,5 +1,5 @@
 // const couchbase = require('couchbase'); // crashing in both netlify and vercel
-const { MongoClient } = require("mongodb");
+const { MongoClient } = require("mongodb"); // not crashing
 
 module.exports = (req, res) => {
     res.status(200).json({ message: 'Hello from Vercel Function!' });
